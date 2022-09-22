a7469dc81468090a81f0fed93abbc10c0b7b1079
AqidaHaidari <aghida55@gmail.com>
2022-09-22 18:58:54 +0430

    initial commit

---
+++ src/Select.tsx
@@ -0,0 +1,27 @@
+import styles from './select.module.css'
+
+type selectOption={
+    label:String
+    value:any
+}
+
+type selectProps={
+    options:selectOption[]
+    value?:selectOption
+    onChange:(value:selectOption | undefined)=>void
+}
+export function Select({value, onChange, options}:selectProps){
+    return (<div tabIndex={0} className={styles.container}>
+        <span className={styles.value}>Value</span>
+        <button className={styles['clear-btn']}>&times;</button>
+        <div className={styles.devider}></div>
+        <div className={styles.caret}></div>
+        <ul className={styles.options}>
+            {options.map(option=>(
+                <li key={option.label} className={styles.option}>{option.label}</li>
+            ))}
+        </ul>
+    </div>)
+   
+    
+}
